bca5631c57f2e72c364f49dbf34820234aee83c3
井川司 <tsukasa.ikawa@secual-inc.com>
2023-03-02 13:20:08 +0900

    update: digestRequest.go
    respond SHA256 and SHA512
    deal with non double quote

--- digestRequest.go
+++ digestRequest.go
@@ -2,13 +2,14 @@ package digestRequest
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
 
-	"github.com/delphinus/random-string"
 	"golang.org/x/net/context"
 )
 
@@ -23,9 +24,11 @@ func ContextWithClient(parent context.Context, client *http.Client) context.Cont
 }
 
 func clientFromContext(ctx context.Context) *http.Client {
+
 	if client, ok := ctx.Value(HTTPClientKey).(*http.Client); ok {
 		return client
 	}
+
 	return http.DefaultClient
 }
 
@@ -40,23 +43,25 @@ type DigestRequest struct {
 type nonceCount int
 
 func (nc nonceCount) String() string {
+
 	c := int(nc)
 	return fmt.Sprintf("%08x", c)
 }
 
 const algorithm = "algorithm"
-const authorization = "Authorization"
-const contentType = "Content-Type"
 const nonce = "nonce"
 const opaque = "opaque"
 const qop = "qop"
 const realm = "realm"
-const wwwAuthenticate = "Www-Authenticate"
 
-var wanted = []string{algorithm, nonce, opaque, qop, realm}
+const wwwAuthenticate = "Www-Authenticate"
+const authorization = "Authorization"
+
+var wanted = []string{nonce, qop, realm}
 
 // New makes a DigestRequest instance
 func New(ctx context.Context, username, password string) *DigestRequest {
+
 	return &DigestRequest{
 		Context:  ctx,
 		client:   clientFromContext(ctx),
@@ -67,6 +72,7 @@ func New(ctx context.Context, username, password string) *DigestRequest {
 
 // Do does requests as http.Do does
 func (r *DigestRequest) Do(req *http.Request) (*http.Response, error) {
+
 	parts, err := r.makeParts(req)
 	if err != nil {
 		return nil, err
@@ -80,11 +86,17 @@ func (r *DigestRequest) Do(req *http.Request) (*http.Response, error) {
 }
 
 func (r *DigestRequest) makeParts(req *http.Request) (map[string]string, error) {
+
 	authReq, err := http.NewRequest(req.Method, req.URL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
 	resp, err := r.client.Do(authReq)
 	if err != nil {
 		return nil, err
 	}
+
 	defer func() { _ = resp.Body.Close() }()
 
 	if resp.StatusCode != http.StatusUnauthorized {
@@ -97,10 +109,17 @@ func (r *DigestRequest) makeParts(req *http.Request) (map[string]string, error) 
 
 	headers := strings.Split(resp.Header[wwwAuthenticate][0], ",")
 	parts := make(map[string]string, len(wanted))
+
 	for _, r := range headers {
+
 		for _, w := range wanted {
+
 			if strings.Contains(r, w) {
-				parts[w] = strings.Split(r, `"`)[1]
+
+				kv := strings.Split(r, "=")
+				if len(kv) > 1 {
+					parts[w] = strings.Trim(kv[1], `"`)
+				}
 			}
 		}
 	}
@@ -109,33 +128,70 @@ func (r *DigestRequest) makeParts(req *http.Request) (map[string]string, error) 
 		return nil, fmt.Errorf("header is invalid: %+v", parts)
 	}
 
+	aval, ok := parts[algorithm]
+	if ok {
+
+		if strings.HasSuffix(aval, "-sess") {
+			return nil, fmt.Errorf("not support session variant: %s", aval)
+		}
+	}
+
+	qval := parts[qop]
+	if qval != "auth" {
+		return nil, fmt.Errorf("not support quality of protection: %s", qval)
+	}
+
+	_, ok = parts[algorithm]
+	if !ok {
+		parts[algorithm] = "MD5"
+	}
+
 	return parts, nil
 }
 
-func getMD5(texts []string) string {
-	h := md5.New()
-	_, _ = io.WriteString(h, strings.Join(texts, ":"))
-	return hex.EncodeToString(h.Sum(nil))
-}
-
 func (r *DigestRequest) getNonceCount() string {
+
 	r.nonceCount++
 	return r.nonceCount.String()
 }
 
 func (r *DigestRequest) makeAuthorization(req *http.Request, parts map[string]string) string {
-	ha1 := getMD5([]string{r.username, parts[realm], r.password})
-	ha2 := getMD5([]string{req.Method, req.URL.String()})
-	cnonce := randomString.Generate(16)
+
+	cnonce := Generate(16)
 	nc := r.getNonceCount()
-	response := getMD5([]string{
-		ha1,
-		parts[nonce],
-		nc,
-		cnonce,
-		parts[qop],
-		ha2,
-	})
+	var response string
+
+	switch strings.ToLower(parts[algorithm]) {
+	case "sha256":
+		ha1 := getSHA256([]string{r.username, parts[realm], r.password})
+		ha2 := getSHA256([]string{req.Method, req.URL.String()})
+		response = getSHA256([]string{ha1, parts[nonce], nc, cnonce, parts[qop], ha2})
+	case "sha512":
+		ha1 := getSHA512([]string{r.username, parts[realm], r.password})
+		ha2 := getSHA512([]string{req.Method, req.URL.String()})
+		response = getSHA512([]string{ha1, parts[nonce], nc, cnonce, parts[qop], ha2})
+	default:
+		ha1 := getMD5([]string{r.username, parts[realm], r.password})
+		ha2 := getMD5([]string{req.Method, req.URL.String()})
+		response = getMD5([]string{ha1, parts[nonce], nc, cnonce, parts[qop], ha2})
+	}
+
+	oval, ok := parts[opaque]
+	if !ok {
+		return fmt.Sprintf(
+			`Digest username="%s", realm="%s", nonce="%s", uri="%s", algorithm=%s, qop=%s, nc=%s, cnonce="%s", response="%s"`,
+			r.username,
+			parts[realm],
+			parts[nonce],
+			req.URL.String(),
+			parts[algorithm],
+			parts[qop],
+			nc,
+			cnonce,
+			response,
+		)
+	}
+
 	return fmt.Sprintf(
 		`Digest username="%s", realm="%s", nonce="%s", uri="%s", algorithm=%s, qop=%s, nc=%s, cnonce="%s", response="%s", opaque="%s"`,
 		r.username,
@@ -147,6 +203,27 @@ func (r *DigestRequest) makeAuthorization(req *http.Request, parts map[string]st
 		nc,
 		cnonce,
 		response,
-		parts[opaque],
+		oval,
 	)
+}
+
+func getMD5(texts []string) string {
+
+	h := md5.New()
+	_, _ = io.WriteString(h, strings.Join(texts, ":"))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func getSHA256(texts []string) string {
+
+	h := sha256.New()
+	_, _ = io.WriteString(h, strings.Join(texts, ":"))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func getSHA512(texts []string) string {
+
+	h := sha512.New()
+	_, _ = io.WriteString(h, strings.Join(texts, ":"))
+	return hex.EncodeToString(h.Sum(nil))
 }
